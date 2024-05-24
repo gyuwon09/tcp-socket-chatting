4bc88ba11134d4eafb10022e189cad098df4e5a8
gyuwon09 <133576303+gyuwon09@users.noreply.github.com>
2024-05-24 18:38:48 +0900

    translation to english and add comment

--- client.py
+++ client.py
@@ -9,12 +9,13 @@ class tcpclient():
                 pass
 
         def connect(self):
+                #client socket create
                 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 client_socket.connect((host, port))
-
+                #set to receive message from server to threading
                 server_receiver = threading.Thread(target=self.receive_chat, args=(client_socket,))
                 server_receiver.start()
-
+                #set to send message from server to threading
                 server_sender = threading.Thread(target=self.send_chat, args=(client_socket,))
                 server_sender.start()
 
@@ -40,37 +41,3 @@ class tcpclient():
 
 chatting_client = tcpclient()
 chatting_client.connect()
-
-"""
-def receive_messages(client_socket):
-        while True:
-                try:
-                        # 서버로부터 메시지 수신
-                        message = client_socket.recv(1024).decode('utf-8')
-                        if not message:
-                                print("서버가 연결을 종료했습니다.")
-                                break
-                        print(f"[server]: {message}")
-                except ConnectionResetError:
-                        print("서버 연결이 강제로 종료되었습니다.")
-                        break
-
-def main():
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_socket.connect((host, port))
-
-        receiver_thread = threading.Thread(target=receive_messages, args=(client_socket,))
-        receiver_thread.start()
-
-        while True:
-                # 서버에게 메시지 전송
-                message = input("클라이언트에서 보낼 메시지: ")
-                if message == 'quit':
-                        break
-                client_socket.send(message.encode('utf-8'))
-
-        client_socket.close()
-
-if __name__ == "__main__":
-    main()
-"""
--- server.py
+++ server.py
@@ -9,16 +9,20 @@ class tcpserver():
         def open(self):
                 host = os.getenv('host')
                 port = int(os.getenv('port'))
-                server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #tcp 연결소켓 생성
-                server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #소켓 옵션을 설정하여 이미 사용된 주소를 재사용할 수 있게 합니다.
+                #tcp socket create
+                server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+                #Set the socket option to allow reuse of an already used address
+                server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
                 server_socket.bind((host, port))
                 server_socket.listen()
                 print(f"[*] ip : {host} port : {port}")
                 while True:
                         client_socket, addr = server_socket.accept()
-                        print(f"[+] {addr} connected.")        
+                        print(f"[+] {addr} connected.")
+                        #set to receive message from client to thread
                         client_receive = threading.Thread(target=self.receive_chat, args=(client_socket,))
                         client_receive.start()
+                        #set to send message from client to thread
                         client_sender = threading.Thread(target=self.send_chat, args=(client_socket,))
                         client_sender.start()
 
@@ -41,45 +45,6 @@ class tcpserver():
                                 client_socket.send("Error 404".encode('utf-8'))
                                 break
                 client_socket.close()
-        
 
 chatting_server = tcpserver()
 chatting_server.open()
-
-"""
-def handle_client(client_socket):
-    while True:
-        try:
-            message = client_socket.recv(1024).decode('utf-8')
-            if not message:
-                print("클라이언트가 연결을 종료했습니다.")
-                break
-            print(f"[client]: {message}")
-            
-            # 클라이언트에게 메시지 전송
-            response = input("서버에서 보낼 메시지: ")
-            client_socket.send(response.encode('utf-8'))
-        except ConnectionResetError:
-            print("클라이언트 연결이 강제로 종료되었습니다.")
-            break
-
-    client_socket.close()
-
-def main():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    host = os.getenv('host')
-    port = int(os.getenv('port'))
-    server_socket.bind((host, port))
-    server_socket.listen()
-    print(f"[*] ip : {host} port : {port}")
-
-    while True:
-        client_socket, addr = server_socket.accept()
-        print(f"[+] {addr} connected.")
-        client_handler = threading.Thread(target=handle_client, args=(client_socket,))-
-        client_handler.start()
-
-if __name__ == "__main__":
-    main()
-"""
